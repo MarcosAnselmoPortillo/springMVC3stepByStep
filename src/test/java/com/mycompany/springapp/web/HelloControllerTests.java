9af32ddad1d6e98a0ffb70c78b6c12afce6e8ac3
Marcos Portillo <marcos.portillo@wnco.com>
2014-07-30 16:17:10 -0500

    primer commit

---
+++ src/test/java/com/mycompany/springapp/web/HelloControllerTests.java
@@ -0,0 +1,17 @@
+package com.mycompany.springapp.web
+
+import com.mycompany.springapp.web.HelloController;
+import org.junit.Test
+import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.Exception;
+
+public class HelloControllerTests{
+
+    @Test
+    public void testHandeRequestView() throws Exception{
+        HelloController helloController = new HelloController();
+        ModelAndView modelAndView = helloController.handleRequest(null,null);
+        assertEquals("hello.jsp",modelAndView.getViewName());
+    }
+}
