eea20777ae1d837ae569f855d9a509e0fa72d20c
Marcos Portillo <marcos.portillo@wnco.com>
2014-08-04 13:12:26 -0500

    6.1. Modificar la Capa de Servicio

--- src/main/java/com/mycompany/springapp/service/SimpleProductManager.java
+++ src/main/java/com/mycompany/springapp/service/SimpleProductManager.java
@@ -1,33 +1,40 @@
 package com.mycompany.springapp.service;
 
 import com.mycompany.springapp.domain.Producto;
+import com.mycompany.springapp.repository.ProductoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
  * Created by developer on 7/31/14.
  */
+@Component
 public class SimpleProductManager implements ProductManager {
 
     private static final long serialVersionUID=1L;
 
-    private List<Producto> productos;
+    @Autowired
+    private ProductoDao productoDao;
+
+    public void setProductoDao(ProductoDao productoDao) {
+        this.productoDao = productoDao;
+    }
 
     public List<Producto> getProductos(){
-        return productos;
+        return productoDao.getListaProductos();
     }
 
     public void incrementarPrecio(int porcentaje){
+        List<Producto> productos = productoDao.getListaProductos();
         if (productos != null){
             for(Producto producto : productos){
                 double newPrice = producto.getPrecio().doubleValue()*(100+ porcentaje)/100;
                 producto.setPrecio(newPrice);
+                productoDao.saveProducto(producto);
             }
         }
     }
 
-
-    public void setProductos(List<Producto> productos){
-        this.productos=productos;
-    }
 }
